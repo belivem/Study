df541536d22fbcd3e796a4be6cbb43029757a7ba
belivem <toward-behind@163.com>
2017-11-22 21:48:11 +0800

    完成 张量概念

--- src/tesorflow_test/tensor_example.py
+++ src/tesorflow_test/tensor_example.py
@@ -7,3 +7,11 @@ Created on Tue Nov 21 23:24:36 2017
 """
 
 import tensorflow as tf
+
+a = tf.constant([1.0,2.0],name = "c")
+b = tf.constant([3.0,4.0],dtype=tf.float32,name = "d")
+
+result = tf.add(a,b,name="sum")
+
+with tf.Session() as sess:
+    print(sess.run(result))
