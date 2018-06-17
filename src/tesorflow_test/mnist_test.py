58c6441bdeb5a47d36e2a04ec86b9d5b737fcf1a
Yanan Li <toward-behind@163.com>
2018-06-17 17:26:53 +0800

    fully_connected_network!

--- src/tesorflow_test/mnist_test.py
+++ src/tesorflow_test/mnist_test.py
@@ -4,8 +4,7 @@ import neural_util
 
 mnist_path = "/Users/liyanan/Documents/Test/Tensorflow/src/tesorflow_test/data/mnist_data/"
 
-
-def neural_netword():
+def fully_connected_netword():
 
     mnist_data = mnist.getmnist(mnist_path)
     #print the num_examples of training data
@@ -16,7 +15,11 @@ def neural_netword():
     input_size = 784
     hidden_size = 500
     output_size = 10
-    
+    regularization_rate = 0.0001
+    learning_rate_base = 0.8
+    learning_rate_decay = 0.99
+    moving_average_decay = 0.99
+
     #define input data
     input_x = tf.placeholder(tf.float32,shape=[None,input_size],name="input_x")
     input_y = tf.placeholder(tf.float32,shape=[None,output_size],name="input_y")
@@ -32,15 +35,30 @@ def neural_netword():
 
     #define loss function  ==> cross_entroy = -tf.reduce_sum(input_y * tf.log(tf.clip_by_value(softmax_y,1e-10,1)),axis=1) 
     cross_entroy = tf.nn.softmax_cross_entropy_with_logits(logits=layer2,labels=input_y)
-    loss = tf.reduce_mean(cross_entroy)
+    l2_regularizer = tf.contrib.layers.l2_regularizer(regularization_rate)(weigths1)+tf.contrib.layers.l2_regularizer(regularization_rate)(weigths2)
+    loss = tf.reduce_mean(cross_entroy)+l2_regularizer
+    
 
-    #dfine training process
-    train_op = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss)
+    #dfine training process ,set decay learning_rate[指数衰减法] and moving_average[滑动平均模型]
+    #set decay learning rate
+    global_steps = tf.Variable(0,trainable=False)
+    learning_rate = tf.train.exponential_decay(learning_rate_base,global_step=global_steps,decay_steps=100,decay_rate=learning_rate_decay,name="learning_rate_dynamic")
+    
+    #set moving average
+    ema = tf.train.ExponentialMovingAverage(decay=moving_average_decay,num_updates=global_steps)
+    moving_average_op = ema.apply(tf.trainable_variables())
 
-    #define the measures
-    layer2_output = neural_util.get_softmax_logits(input_x,weigths1,weigths2,biases1,biases2,2)
-    correct_prediction = tf.equal(tf.argmax(layer2_output,1),tf.argmax(input_y,1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+    #set train operation
+    train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss,global_step=global_steps)
+    train_op = tf.group(train_step,moving_average_op)
+
+    #Get the measures
+    #layer2_output = neural_util.get_softmax_logits(input_x,weigths1,weigths2,biases1,biases2,2)
+    #correct_prediction = tf.equal(tf.argmax(layer2_output,1),tf.argmax(input_y,1))
+    #accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+    actual_labels = tf.argmax(input_y,1)
+    predict_labels = tf.argmax(neural_util.get_softmax_logits(input_x,weigths1,weigths2,biases1,biases2,2),1)
+    (precision,recall,f1,mean_precision,mean_recall,mean_f1) = neural_util.getMeasures(actual_labels,predict_labels)
 
     init = (tf.global_variables_initializer(),tf.local_variables_initializer())
 
@@ -54,12 +72,17 @@ def neural_netword():
             if i % 1000 == 0:
                 batch_loss_export = sess.run(loss,feed_dict={input_x:batch_x,input_y:batch_y})
                 all_loss_exoprt = sess.run(loss,feed_dict={input_x:mnist_data.train.images,input_y:mnist_data.train.labels})
-                batch_accuracy = sess.run(accuracy,feed_dict={input_x:batch_x,input_y:batch_y})
-                all_accuracy = sess.run(accuracy,feed_dict={input_x:mnist_data.train.images,input_y:mnist_data.train.labels})
+
+                measures = sess.run((precision,recall,f1,mean_precision,mean_recall,mean_f1),feed_dict={input_x:mnist_data.validation.images,input_y:mnist_data.validation.labels})
+
+                print("After of "+str(i)+" training, the loss of batch_data is "+str(batch_loss_export)+", and the loss of all_data is "+str(all_loss_exoprt)+  
+                ", mean_precision is "+str(measures[3])+", mean_recall is "+str(measures[4])+", mean_f1 is "+str(measures[5]))
+
+            if i == (training_step - 1):
                 print("After of "+str(i)+" training, the loss of batch_data is "+str(batch_loss_export)+", and the loass of all_data is "+str(all_loss_exoprt)+  
-                ", the accuracy of batch_data is "+str(batch_accuracy)+", and the accuracy os all_data is "+str(all_accuracy))
-                                
+                    ", precision is "+str(measures[0])+", recall is "+str(measures[1])+", f1 is "+str(measures[2])+", mean_precision is "+str(measures[3])+", mean_recall is "
+                    +str(measures[4])+", mean_f1 is "+str(measures[5]))                    
 
 
 if __name__ == "__main__":
-    neural_netword()
+    fully_connected_netword()
