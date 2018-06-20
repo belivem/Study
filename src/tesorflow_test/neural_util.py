d0b103adcc721fa2e69d592c1a76c5a92886bd50
Yanan Li <toward-behind@163.com>
2018-06-20 21:42:09 +0800

    load pb model

--- src/tesorflow_test/neural_util.py
+++ src/tesorflow_test/neural_util.py
@@ -32,3 +32,18 @@ def getMeasures(actual_labels,predict_labels):
     mean_f1 = tf.reduce_mean(f1)
 
     return (precision,recall,f1,mean_precision,mean_recall,mean_f1)
+
+#load the persistence pb model
+def load_pb_graph(filename):
+
+    if not tf.gfile.Exists(filename):
+        print(filename+" is not existd!")
+        return None
+
+    with tf.gfile.GFile(filename,mode="rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+    
+    tf.import_graph_def(graph_def,name="importPB")
+    graph = tf.get_default_graph()
+    return graph
